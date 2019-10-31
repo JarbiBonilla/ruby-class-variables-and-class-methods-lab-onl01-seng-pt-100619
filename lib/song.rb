2d07f585f1730630b5e1f5c28c8e3481ef44e3ad
Jarbi Bonilla <jarbibonilla@gmail.com>
2019-10-31 03:18:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,8 +32,6 @@ class Song
     @@genres.each do |genre|
       if genre_count[genre]
         genre_count[genre] += 1 
-      else
-        genre_count[genre] = 1
       end
     end
     genre_count
